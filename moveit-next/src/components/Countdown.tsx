60a1b686b08f2c10b40a38257a91aab99bb0f479
ghblue <ghresilva@.inf.ufpel.edu.br>
2021-04-14 16:49:35 -0300

    New functionalities

--- moveit-next/src/components/Countdown.tsx
+++ moveit-next/src/components/Countdown.tsx
@@ -1,57 +1,25 @@
 import { useState, useEffect, useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 //useEffect função para disparar efeitos colaterais quando algo acontecer
 import styles from '../styles/components/Countdown.module.css'
 
-//variável pra garantir a parada instantanea do countdown (javascript puro)
-let countdownTimeout: NodeJS.Timeout;
+
 
 export function Countdown() {
-    
-    const { startNewChallenge } = useContext(ChallengesContext);
-    
-    //useState
-    const [time, setTime] = useState(0.05*60);
-
-    //esse estado armazena se o countdown está ativo ou não
-    const [isActive, setIsActive] = useState(false);
-
-    const [hasFinished, setHasFinished] = useState(false);
-
-    //Math.floor() arredonda pra baixo
-    const minutes = Math.floor(time / 60)
-    const seconds = time % 60;
+    const { minutes,
+        seconds,
+        hasFinished, 
+        isActive, 
+        startCountdown, 
+        resetCountdown
+        } = useContext(CountdownContext)
     
     /*o minuteLeftRight verifica se nossa string tem 2 caracteres, e se tiver só 1, completa a parte esquerda com zero */ 
     const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
     const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
 
-    function startCountdown(){
-        setIsActive(true);
-    }
-
-    function resetCountdown(){
-        clearTimeout(countdownTimeout);
-        setIsActive(false);
-        setTime(0.5 * 60);
-    }
-
-
-    //useEffect (function, quando executar)
-    //javascript puro
-    useEffect(() =>{
-        if(isActive && time > 0){
-            countdownTimeout = setTimeout(() => {
-                //recursão para a contagem não parar
-                setTime(time - 1)
-            }, 1000)
-        } else if(isActive && time == 0){
-            setHasFinished(true);
-            setIsActive(false);
-            startNewChallenge();
-        }
-    }, [isActive, time] )
-
+    
     return(
         <div>
             <div className={styles.countdownContainer}>
--- moveit-next/src/contexts/ChallengesContext.tsx
+++ moveit-next/src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from 'react'
+import { createContext, useState, ReactNode, useEffect } from 'react'
 import challenges from '../../challenges.json'
 
 interface Challenge{
@@ -17,6 +17,7 @@ interface ChallengesContextData{
     resetChallenge: () => void;
     levelUp: () => void;
     startNewChallenge: () => void;
+    completeChallenge: () => void;
 }
 
 interface ChallengesProviderProps{
@@ -27,12 +28,16 @@ export const ChallengesContext = createContext({} as ChallengesContextData );
 
 export function ChallengesProvider({ children }: ChallengesProviderProps){
     const [level, setLevel] = useState(1);
-    const [currentExperience, setCurrentExperience] = useState(30);
+    const [currentExperience, setCurrentExperience] = useState(0);
     const [challengesCompleted, setChallengesCompleted] = useState(0);
     
     const [activeChallenge, setActiveChallenge] = useState(null);
 
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
+
+    useEffect(() =>{
+        Notification.requestPermission();
+    }, [])
 
     function levelUp(){
         setLevel(level + 1);
@@ -44,10 +49,36 @@ export function ChallengesProvider({ children }: ChallengesProviderProps){
         const challenge = challenges[randomChallengeIndex];
 
         setActiveChallenge(challenge)
+
+        new Audio('/notification.mp3').play();
+
+        if(Notification.permission === 'granted'){
+            new Notification('Novo desafio !!', {
+                body: `Valendo ${challenge.amount}xp!`
+            })
+        }
     }
 
     function resetChallenge(){
         setActiveChallenge(null);
+    }
+
+    function completeChallenge(){
+        if(!activeChallenge){
+            return;
+        }
+        
+        const { amount } = activeChallenge;
+
+        let finalExperience = currentExperience + amount;
+
+        if(finalExperience >= experienceToNextLevel){
+            finalExperience = finalExperience - experienceToNextLevel;
+            levelUp()
+        }
+        setCurrentExperience(finalExperience)
+        setActiveChallenge(null)
+        setChallengesCompleted(challengesCompleted + 1)
     }
 
     return (
@@ -58,6 +89,7 @@ export function ChallengesProvider({ children }: ChallengesProviderProps){
             activeChallenge,
             experienceToNextLevel,
             resetChallenge,
+            completeChallenge,
             startNewChallenge, 
             levelUp}}>
             {children}
